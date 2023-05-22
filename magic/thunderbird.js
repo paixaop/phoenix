1bf7818770b485c7d1365e163bc281d4597b3476
Pedro Paixao <paixaop@gmail.com>
2023-05-21 22:51:36 -0400

    center thunderbird write window on the same screen as main window

--- magic/thunderbird.js
+++ magic/thunderbird.js
@@ -16,5 +16,21 @@ function magicThunderbirdOpen ( window ) {
   if ( !/Write/.test(windowTitle) ) return;
   Phoenix.log(`Center Thunderbird writing window`);
 
-  centerWindow (window);
+  for(const w of window.app().windows()) {
+    Phoenix.log(`Thunderbird ${w.title}`);
+    if ( !/Write/.test(w.title()) ) {
+      const sFrame = w.screen().flippedVisibleFrame();
+      const wFrame = window.frame();
+      window.setFrame ({
+        x: sFrame.x + ( sFrame.width / 2 ) - ( wFrame.width / 2 ),
+        y: sFrame.y + ( sFrame.height / 2 ) - ( wFrame.height / 2 ),
+        width: wFrame.width,
+        height: wFrame.height
+      });
+      return;
+    }
+  }
+
+  centerWindow(window);
+  
 }
